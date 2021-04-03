754a00bd6d7a7b683783fc3448ea8f0f26193926
Antti Laiti <antti@exove.fi>
2021-04-03 15:11:29 +0300

    Write first tests

--- cypress/integration/ratebeer.js
+++ cypress/integration/ratebeer.js
@@ -1,6 +1,11 @@
 describe('My First Test', () => {
     it('Does not do much!', () => {
-      expect(true).to.equal(true)
+        cy.visit('https://ratebeer.com')
+        cy.contains('Log In').click();
+        cy.get('input[name=username]').type('Hello, World');
+        cy.get('input[name=password]').type('Hello, World');
+        cy.contains('Accept All Cookies').click();
+        cy.get('#auth0-label-submit').click();
     })
   })
   
