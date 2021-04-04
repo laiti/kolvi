0b2b2b390b298ada1f301fbe128bf739d421dfd5
Antti Laiti <antti@laiti.fi>
2021-04-04 19:02:00 +0300

    Fix env var and update README

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # kolvi
-Cypress.io project
+Cypress.io project to input scores from CSV to ratebeer.com.
+
+### Quick start guide
+- Set environment variables: `CYPRESS_RB_USERNAME` and `CYPRESS_RB_PASSWORD`
+- Run `npm run cypress`
+- Click ratebeer.js
--- cypress/integration/ratebeer.js
+++ cypress/integration/ratebeer.js
@@ -1,9 +1,9 @@
 describe('My First Test', () => {
-    it('Does not do much!', () => {
-        cy.visit('https://ratebeer.com')
+    it('Login to Ratebeer', () => {
+        cy.visit('https://ratebeer.com/')
         cy.contains('Log In').click();
-        cy.get('input[name=username]').type(process.env['RB_USERNAME']);
-        cy.get('input[name=password]').type(process.env['RB_PASSWORD']);
+        cy.get('input[name=username]').type(Cypress.env('RB_USERNAME'));
+        cy.get('input[name=password]').type(Cypress.env('RB_PASSWORD'));
         cy.contains('Accept All Cookies').click();
         cy.get('.auth0-label-submit').contains('Log In').click();
     })
