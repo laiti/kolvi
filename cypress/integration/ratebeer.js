3b2e1a68d32fc38b2fe234d849bd304bb072ce46
Antti Laiti <antti@laiti.fi>
2021-04-04 18:50:35 +0300

    Use env vars for login

--- cypress/integration/ratebeer.js
+++ cypress/integration/ratebeer.js
@@ -2,8 +2,8 @@ describe('My First Test', () => {
     it('Does not do much!', () => {
         cy.visit('https://ratebeer.com')
         cy.contains('Log In').click();
-        cy.get('input[name=username]').type('Hello, World');
-        cy.get('input[name=password]').type('Hello, World');
+        cy.get('input[name=username]').type(process.env['RB_USERNAME']);
+        cy.get('input[name=password]').type(process.env['RB_PASSWORD']);
         cy.contains('Accept All Cookies').click();
         cy.get('.auth0-label-submit').contains('Log In').click();
     })
