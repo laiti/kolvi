8eaa0f57fc705064c005b409c7dd2bbdd0b9a73b
Antti Laiti <antti@laiti.fi>
2021-04-12 16:54:24 +0300

    Attempt to read CSV directly

--- cypress/integration/ratebeer.js
+++ cypress/integration/ratebeer.js
@@ -1,7 +1,5 @@
 import csv from 'csv-parser';
-const fs = require('fs');
-
-const RBURL = 'https://ratebeer.com';
+const fs = require('data/ratebeer.js');
 
 // TEMP: Variables from CSV
 const format = 'Can';
@@ -10,6 +8,8 @@ const appearance = 2;
 const flavor = 3;
 const mouthfeel = 4;
 const overall = 5;
+
+const rbdata = fs.createReadStream('data/ratebeer.csv');
 
 // Slider classes
 const sliderClasses = {
@@ -30,7 +30,7 @@ const sliderDivClass = 'div > div[class="Slider___StyledDiv-kfCDlB dxmuNj"] > di
 
 describe('Login to Ratebeer and post rating', () => {
     it('Login to Ratebeer', () => {
-        cy.visit(RBURL)
+        cy.visit('https://ratebeer.com/');
         cy.contains('Log In').click();
         cy.get('input[name=username]').type(Cypress.env('RB_USERNAME'));
         cy.get('input[name=password]').type(Cypress.env('RB_PASSWORD'));
@@ -38,22 +38,31 @@ describe('Login to Ratebeer and post rating', () => {
         cy.get('.auth0-label-submit').contains('Log In').click();
         cy.contains('Log In').should('not.exist');
 
-        cy.visit(`${RBURL}/beer/pfriem-vienna-lager/479195/`);
-        cy.contains('Write a review').click();
-        // Multiple textareas visible; correct one is with this class and aria-invalid attribute.
-        // The elements in rating view can be hidden so we must use force.
-        cy.get('textarea[class^="MuiInputBase-input"][aria-invalid=false]').type('foo', { force: true });
+        // Read CSV and add each rating in a loop
+        rbdata.pipe(csv()).on('data', (rating) => {
 
-        // Sliders; rather complex nested div section
-        cy.get(`div[data-testid="attribute-slider-aroma"] > ${sliderDivClass} ${sliderClasses[aroma]}"]`).click({ force: true });
-        cy.get(`div[data-testid="attribute-slider-appearance"] > ${sliderDivClass} ${sliderClasses[appearance]}"]`).click({ force: true });
-        cy.get(`div[data-testid="attribute-slider-flavor"] > ${sliderDivClass} ${sliderClasses[flavor]}"]`).click({ force: true });
-        cy.get(`div[data-testid="attribute-slider-mouthfeel"] > ${sliderDivClass} ${sliderClasses[mouthfeel]}"]`).click({ force: true });
-        cy.get(`div[data-testid="attribute-slider-overall"] > ${sliderDivClass} ${sliderClasses[overall]}"]`).click({ force: true });
+            cy.visit(`https://ratebeer.com/beer/pfriem-vienna-lager/479195/`);
+            cy.contains('Write a review').click();
+        
+            // Multiple textareas visible; correct one is with this class and aria-invalid attribute.
+            // The elements in rating view can be hidden so we must use force.
+            cy.get('textarea[class^="MuiInputBase-input"][aria-invalid=false]').type('foo', { force: true });
 
-        // Format the drink was served in
-        cy.get('span').contains(format).click({ force: true });
-        // cy.get('span').contains('Save').click();
+            // Sliders; rather complex nested div section
+            cy.get(`div[data-testid="attribute-slider-aroma"] > ${sliderDivClass} ${sliderClasses[aroma]}"]`).click({ force: true });
+            cy.get(`div[data-testid="attribute-slider-appearance"] > ${sliderDivClass} ${sliderClasses[appearance]}"]`).click({ force: true });
+            cy.get(`div[data-testid="attribute-slider-flavor"] > ${sliderDivClass} ${sliderClasses[flavor]}"]`).click({ force: true });
+            cy.get(`div[data-testid="attribute-slider-mouthfeel"] > ${sliderDivClass} ${sliderClasses[mouthfeel]}"]`).click({ force: true });
+            cy.get(`div[data-testid="attribute-slider-overall"] > ${sliderDivClass} ${sliderClasses[overall]}"]`).click({ force: true });
+
+            // Format the drink was served in
+            cy.get('span').contains(format).click({ force: true });
+            // cy.get('span').contains('Save').click();
+        })
+        .on('end', () => {
+            console.log('CSV file successfully processed');
+        });
+            
     })
 })
   
